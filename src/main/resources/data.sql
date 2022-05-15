f9bf32dadb406dc6744c255143e760bc17aa76c6
Giridhar Kandachar <giridharkl@gmail.com>
2022-05-15 18:15:54 +0300

    adding data.sql populating init data to tables

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into continent (id,name) values (1,'Asia');
+insert into continent(id,name) values(2,'Africa');
+insert into continent(id,name) values(3,'Antarctica');
+insert into continent(id,name) values(4,'Australia');
+insert into continent(id,name) values(5,'Europe');
+insert into continent(id,name) values(6,'North America');
+insert into continent(id,name) values(7,'South America');
