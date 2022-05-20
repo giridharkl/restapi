5511f3e93d44d5997540aa68b4f4cddd999b80d2
Giridhar Kandachar <giridharkl@gmail.com>
2022-05-20 22:27:48 +0300

    fixed the issue with sequences for data initialization

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,11 +7,14 @@ insert into continent(id,name) values(4,'Australia');
 insert into continent(id,name) values(5,'Europe');
 insert into continent(id,name) values(6,'North America');
 insert into continent(id,name) values(7,'South America');
+alter sequence continent_seq restart with 8;
 /* Countries */
 insert into country(id,name) values(1,'USA');
 insert into country(id,name) values(2,'Finland');
 insert into country(id,name) values(3,'Germany');
+alter sequence country_seq restart with 4;
 /* Cities */
 insert into city(id,name,continent_id,country_id) values(1,'Helsinki',5,2);
 insert into city(id,name,continent_id,country_id) values(2,'Frakfurt',5,3);
 insert into city(id,name,continent_id,country_id) values(3,'NewYork',6,1);
+alter sequence city_seq restart with 4;
